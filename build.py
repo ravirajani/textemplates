5e32ac9144c53929cfec5a250832b9f2c2889d94
Ravi Rajani <ravi.inajar@gmail.com>
2025-02-16 11:40:23 +0100

    functioning build command

--- build.py
+++ build.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser, BooleanOptionalAction
 from pathlib import Path
+import subprocess
 
 VERSION_FILE = 'version.txt'
 
@@ -10,18 +11,6 @@ parser.add_argument('files', nargs='+', type=Path)
 parser.add_argument('--major', default=False, action=BooleanOptionalAction)
 parser.add_argument('--minor', default=False, action=BooleanOptionalAction)
 args = parser.parse_args()
-
-build_cmd = {
-    "command": "latexmk",
-    "args": [
-        "-shell-escape",
-        "-synctex=1",
-        "-interaction=nonstopmode",
-        "-file-line-error",
-        "-pdf",
-        "-outdir=out"
-    ]
-}
 
 with open(VERSION_FILE, 'r+') as f:
     version = f.read()
@@ -37,7 +26,21 @@ with open(VERSION_FILE, 'r+') as f:
         f.seek(0)
         f.write(version)
         f.truncate()
+        print('New version:', version)
 
-build_str = f"{build_cmd['command']} {' '.join(build_cmd['args'])}"
 for file in args.files:
-    print(build_str + f' -jobname={file.stem}_v{version}.pdf {file}')
+    outfile = f'{file.stem}_v{version}'
+    build_cmd = [
+        'latexmk',
+        '-shell-escape',
+        '-synctex=1',
+        '-interaction=nonstopmode',
+        '-file-line-error',
+        '-pdf',
+        '-outdir=build',
+        '-jobname='+outfile,
+        str(file)
+    ]
+    p = subprocess.run(build_cmd)
+    if p.returncode == 0:
+        print("Done")
