c0dff6c75b74553bbbebc6375c8f7508a6a6214d
Ravi Rajani <ravi.inajar@gmail.com>
2025-04-18 16:22:16 +0200

    center spines

--- mpl.py
+++ mpl.py
@@ -7,6 +7,12 @@ from matplotlib.backend_bases import register_backend
 register_backend('pdf', FigureCanvasPgf)
 plt.style.use('pgf.mplstyle')
 plt.tight_layout()
+def center_spines(ax=None):
+    if ax is None:
+        ax = plt.gca()
+    for spine in ax.spines.values():
+        spine.set_position(('data', 0))
+    return ax
 # %%
 # Plot Figure
 #==============================================================
